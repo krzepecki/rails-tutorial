fe4a2ea29079efdba352d4bfcbc9160e81f3fbbe
Kacper Rzepecki <kacperrzepecki@MBP-Kacper.localdomain>
2019-05-10 15:10:58 +0200

    Do 8.2.3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
 
-  def hello
-    render html: "Hello world"
+  # Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
   end
+
 end
