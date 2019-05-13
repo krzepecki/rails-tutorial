b40e7df06475477ac12eaa2297b49cb5a4d4206f
Kacper Rzepecki <kacperrzepecki@MBP-Kacper.localdomain>
2019-05-13 18:28:09 +0200

    do 9.1.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-
+attr_accessor :remember_token
 before_save {self.email = self.email.downcase}
 
   validates :name, presence: true, length: {maximum: 50}
@@ -15,6 +15,26 @@ has_secure_password
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  # Returns a random token.
+def User.new_token
+  SecureRandom.urlsafe_base64
+end
+
+# Remembers a user in the database for use in persistent sessions.
+def remember
+  self.remember_token = User.new_token
+  update_attribute(:remember_digest, User.digest(remember_token))
+end
+
+def forget
+  update_attribute(:remember_digest, nil)
+end
+
+# Returns true if the given token matches the digest.
+def authenticated?(remember_token)
+  BCrypt::Password.new(remember_digest).is_password?(remember_token)
+end
 
 end
 
