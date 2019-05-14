1d4aa64ccff922947f7eca9abf7eabb9d6d52a6f
Kacper Rzepecki <kacperrzepecki@MBP-Kacper.localdomain>
2019-05-14 11:28:15 +0200

    Advanced login implemented

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
@@ -10,6 +12,19 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 
   def is_logged_in?
-   !session[:user_id].nil?
+    !session[:user_id].nil?
  end
+
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  # Log in as a particular user.
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
