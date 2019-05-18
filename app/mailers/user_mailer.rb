d3ebafbbbcf527cff3bca8d234a006620b7cbc1a
Kacper Rzepecki <kacperrzepecki@MBP-Kacper.localdomain>
2019-05-18 15:04:16 +0200

    Dodane resetowanie hasła

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,7 +8,7 @@ class UserMailer < ApplicationMailer
   def account_activation(user)
     @user = user
 
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: "Aktywacja konta"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -16,9 +16,9 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.password_reset.subject
   #
-  def password_reset
-    @greeting = "Hi"
+  def password_reset(user)
+    @user = user
 
-    mail to: "to@example.org"
+    mail to: user.email, subject: "Resetowanie hasła"
   end
 end
