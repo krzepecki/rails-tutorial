849ccea450e434b3df5bbe90de54495b956768dd
Kacper Rzepecki <kacperrzepecki@MBP-Kacper.localdomain>
2019-05-22 13:19:23 +0200

    do 14.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,12 @@ has_many :microposts, dependent: :destroy
 has_many :active_relationships, class_name:  "Relationship",
                                 foreign_key: "follower_id",
                                 dependent:   :destroy
+has_many :following, through: :active_relationships, source: :followed
+
+has_many :passive_relationships, class_name:  "Relationship",
+                                 foreign_key: "followed_id",
+                                 dependent:   :destroy
+has_many :followers, through: :passive_relationships, source: :follower
 
 has_secure_password
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
@@ -72,6 +78,21 @@ def password_reset_expired?
 
 def feed
   Micropost.where("user_id = ?", id)
+end
+
+# Follows a user.
+def follow(other_user)
+  following << other_user
+end
+
+# Unfollows a user.
+def unfollow(other_user)
+  following.delete(other_user)
+end
+
+# Returns true if the current user is following the other user.
+def following?(other_user)
+  following.include?(other_user)
 end
 
 private
